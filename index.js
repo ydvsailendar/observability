b3a3b48196ebdc54946d3b3ea5c13f28eba10a23
ydvsailendar <ydvsailendar@gmail.com>
2024-10-17 20:39:54 +0100

    feature: error handling

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT | 4747;
 
+app.use((req, res) => {
+  const errMsg = `Route Not Found: ${req.originalUrl}`;
+  console.warn(errMsg);
+  res.status(404).json({ error: errMsg });
+});
+
+app.use((error, req, res, next) => {
+  console.error(`Server Error: ${error.message}`);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
 app.get("/", (req, res) => {
   res.status(200).send("App Home Page");
 });
@@ -16,6 +27,7 @@ app.get("/health", (req, res) => {
 app.get("/users", async (req, res) => {
   const users = await axios.get(`${process.env.MOCK_API}/users`);
   if (users.data.length === 0) {
+    console.warn("There are no users");
     return res.status(users.status).json(JSON.stringify(users.data));
   }
   res.status(users.status).json(JSON.stringify(users.data));
@@ -28,6 +40,7 @@ app.get("/users/:id", async (req, res) => {
     );
     res.status(user.status).json(JSON.stringify(user.data));
   } catch (err) {
+    console.error(`User with id ${req.params.id} not found`);
     res
       .status(err.toJSON().status)
       .json(`User with id ${req.params.id} not found`);
@@ -37,6 +50,7 @@ app.get("/users/:id", async (req, res) => {
 app.get("/songs", async (req, res) => {
   const songs = await axios.get(`${process.env.MOCK_API}/songs`);
   if (songs.data.length === 0) {
+    console.warn("There are no songs");
     return res.status(songs.status).json(JSON.stringify(songs.data));
   }
   res.status(songs.status).json(JSON.stringify(songs.data));
@@ -49,6 +63,7 @@ app.get("/songs/:id", async (req, res) => {
     );
     res.status(song.status).json(JSON.stringify(song.data));
   } catch (err) {
+    console.error(`Song with id ${req.params.id} not found`);
     res
       .status(err.toJSON().status)
       .json(`Song with id ${req.params.id} not found`);
