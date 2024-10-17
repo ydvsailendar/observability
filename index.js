3385d55d58c2d281d604b1880f0f744237ca90ca
ydvsailendar <ydvsailendar@gmail.com>
2024-10-17 20:57:17 +0100

    feature: add winston for logging

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require("dotenv").config();
 const axios = require("axios");
 const express = require("express");
+const winston = require("winston");
 
 const app = express();
 const PORT = process.env.PORT | 4747;
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [new winston.transports.logger()],
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("App Home Page");
@@ -16,7 +28,7 @@ app.get("/health", (req, res) => {
 app.get("/users", async (req, res) => {
   const users = await axios.get(`${process.env.MOCK_API}/users`);
   if (users.data.length === 0) {
-    console.warn("There are no users");
+    logger.warn("There are no users");
     return res.status(users.status).json(JSON.stringify(users.data));
   }
   res.status(users.status).json(JSON.stringify(users.data));
@@ -29,7 +41,7 @@ app.get("/users/:id", async (req, res) => {
     );
     res.status(user.status).json(JSON.stringify(user.data));
   } catch (err) {
-    console.error(`User with id ${req.params.id} not found`);
+    logger.error(`User with id ${req.params.id} not found`);
     res
       .status(err.toJSON().status)
       .json(`User with id ${req.params.id} not found`);
@@ -39,7 +51,7 @@ app.get("/users/:id", async (req, res) => {
 app.get("/songs", async (req, res) => {
   const songs = await axios.get(`${process.env.MOCK_API}/songs`);
   if (songs.data.length === 0) {
-    console.warn("There are no songs");
+    logger.warn("There are no songs");
     return res.status(songs.status).json(JSON.stringify(songs.data));
   }
   res.status(songs.status).json(JSON.stringify(songs.data));
@@ -52,7 +64,7 @@ app.get("/songs/:id", async (req, res) => {
     );
     res.status(song.status).json(JSON.stringify(song.data));
   } catch (err) {
-    console.error(`Song with id ${req.params.id} not found`);
+    logger.error(`Song with id ${req.params.id} not found`);
     res
       .status(err.toJSON().status)
       .json(`Song with id ${req.params.id} not found`);
@@ -61,13 +73,13 @@ app.get("/songs/:id", async (req, res) => {
 
 app.use((req, res) => {
   const errMsg = `Route Not Found: ${req.originalUrl}`;
-  console.warn(errMsg);
+  logger.warn(errMsg);
   res.status(404).json({ error: errMsg });
 });
 
 app.use((error, req, res, next) => {
-  console.error(`Server Error: ${error.message}`);
+  logger.error(`Server Error: ${error.message}`);
   res.status(500).json({ error: "Internal Server Error" });
 });
 
-app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+app.listen(PORT, () => logger.log(`App running on port ${PORT}`));
