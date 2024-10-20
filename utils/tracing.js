c5e92dca3758d819d473013b7d245e124c13a707
ydvsailendar <ydvsailendar@gmail.com>
2024-10-20 19:06:36 +0100

    feature: add jaeger ui config

--- utils/tracing.js
+++ utils/tracing.js
@@ -1,61 +1,22 @@
-// const opentelemetry = require("@opentelemetry/sdk-node");
-// const {
-//   getNodeAutoInstrumentations,
-// } = require("@opentelemetry/auto-instrumentations-node");
-// const {
-//   OTLPTraceExporter,
-// } = require("@opentelemetry/exporter-trace-otlp-proto");
-// const {
-//   OTLPMetricExporter,
-// } = require("@opentelemetry/exporter-metrics-otlp-proto");
-// const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
-// const { trace, context } = require("@opentelemetry/api");
-
-// const otlpEndpoint = "http://35.179.107.142:4318";
-// const sdk = new opentelemetry.NodeSDK({
-//   serviceName: "mockapi",
-//   traceExporter: new OTLPTraceExporter({
-//     url: `${otlpEndpoint}/v1/traces`,
-//     headers: {},
-//   }),
-//   metricReader: new PeriodicExportingMetricReader({
-//     exporter: new OTLPMetricExporter({
-//       url: `${otlpEndpoint}/v1/metrics`,
-//       headers: {},
-//       concurrencyLimit: 1,
-//     }),
-//     exportIntervalMillis: 10000,
-//   }),
-//   instrumentations: [getNodeAutoInstrumentations()],
-// });
-// sdk.start();
-
-// const getCurrentTraceId = () => {
-//   const span = trace.getSpan(context.active());
-//   return span ? span.spanContext().traceId : null;
-// };
-// module.exports = { getCurrentTraceId };
-
-const { NodeSDK } = require("@opentelemetry/sdk-node");
-const { trace, context } = require("@opentelemetry/api");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
+const opentelemetry = require("@opentelemetry/sdk-node");
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
 const {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} = require("@opentelemetry/sdk-metrics");
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-proto");
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
+const { trace, context } = require("@opentelemetry/api");
 
-const sdk = new NodeSDK({
+const otlpEndpoint = "http://35.179.107.142:4318";
+const sdk = new opentelemetry.NodeSDK({
   serviceName: "mockapi",
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: `${otlpEndpoint}/v1/traces`,
+    headers: {},
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
-
 sdk.start();
 
 const getCurrentTraceId = () => {
